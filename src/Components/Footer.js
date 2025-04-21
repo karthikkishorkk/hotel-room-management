e50f8d3fab183b91ca3c8092ee5ccf619c237fe4
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-04-21 15:59:24 +0530

    Footer Added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,19 +4,19 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-section">
+      <div className="footer-section-one">
         <div className="footer-logo">Booking</div>
         <p>The most memorable moments start with the perfect place to unwind.</p>
       </div>
 
-      <div className="footer-section">
+      <div className="footer-section-two">
         <h4>Quick Links</h4>
         <p>Rooms</p>
         <p>Booking</p>
         <p>Occupancy</p>
       </div>
 
-      <div className="footer-section">
+      <div className="footer-section-three">
         <h4>Contact Us</h4>
         <p>Email: support@booking.com</p>
         <p>Phone: +91 80023 45678</p>
