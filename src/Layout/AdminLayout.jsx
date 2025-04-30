ef00e90c5c0baef4c2d96aa387e5d9a397903530
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-04-30 15:16:45 +0530

    Layout Updated

--- src/Layout/AdminLayout.jsx
+++ src/Layout/AdminLayout.jsx
@@ -44,7 +44,7 @@ const AdminLayout = () => {
         <li onClick={()=>{navigate("/admin")}} >Dashboard</li>
           <li onClick={()=>{navigate("/admin/manage-rooms")}} >Manage Rooms</li>
           <li onClick={()=>{navigate("/admin/manage-users")}}>Manage Staff</li>
-          <li onClick={()=>{navigate("available-rooms")}}>Room Availability</li>
+          {/* <li onClick={()=>{navigate("available-rooms")}}>Room Availability</li> */}
           <li className="logout" onClick={handleLogout}>Logout</li>
         </ul>
       </aside>
